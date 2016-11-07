b957194e6028d607b92b33bf01e35806dbe2f067
jhamil-conaca-coderoad-com <jhamil.conaca@mojix.com>
2016-11-07 19:12:20 -0400

    Initialize project

---
+++ yakukawsay/src/main/java/com/yk/JAXRSConfiguration.java
@@ -0,0 +1,31 @@
+package com.yk;
+
+import com.yk.notification.boundary.NotificationResource;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Configures a JAX-RS endpoint. Delete this class, if you are not exposing
+ * JAX-RS resources in your application.
+ *
+ * @author jhamil
+ */
+@ApplicationPath("resources")
+public class JAXRSConfiguration extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new HashSet();
+        
+        //Currency
+        resources.add(NotificationResource.class);
+
+        //Swagger configuration
+        resources.add(io.swagger.jaxrs.listing.ApiListingResource.class);
+        resources.add(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+        return resources;
+    }
+}
