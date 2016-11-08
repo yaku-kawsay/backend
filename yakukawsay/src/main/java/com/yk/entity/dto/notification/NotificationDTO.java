dad14444c80eac808969ef6668a7031c0f27106a
jhamil-conaca-coderoad-com <jhamil.conaca@mojix.com>
2016-11-08 18:06:04 -0400

    Update configuration files

--- yakukawsay/src/main/java/com/yk/entity/dto/notification/NotificationDTO.java
+++ yakukawsay/src/main/java/com/yk/entity/dto/notification/NotificationDTO.java
@@ -5,6 +5,9 @@
  */
 package com.yk.entity.dto.notification;
 
+import io.swagger.annotations.ApiModelProperty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlRootElement;
