c8935ed30a4827080b79236b109a538114d5d9c0
jhamil-conaca-coderoad-com <jhamil.conaca@mojix.com>
2016-11-08 20:52:35 -0400

    Add load files

---
+++ yakukawsay/src/main/resources/META-INF/sql/data.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  jhamil
+ * Created: Nov 8, 2016
+ */
+
+INSERT INTO `yakukawsay`.`type_indicator` (`id`, `type`) VALUES ('1', 'temperatura');
+INSERT INTO `yakukawsay`.`type_indicator` (`id`, `type`) VALUES ('2', 'humedad');
+INSERT INTO `yakukawsay`.`type_indicator` (`id`, `type`) VALUES ('3', 'nivelAgua');
+
+INSERT INTO `yakukawsay`.`period` (`id`, `filter_time`) VALUES ('1', 'diario');
+INSERT INTO `yakukawsay`.`period` (`id`, `filter_time`) VALUES ('2', 'semanal');
+INSERT INTO `yakukawsay`.`period` (`id`, `filter_time`) VALUES ('3', 'mensual');
+INSERT INTO `yakukawsay`.`period` (`id`, `filter_time`) VALUES ('4', 'anual');
+
